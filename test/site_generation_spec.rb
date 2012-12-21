cba9a6c14f8c1da4619652ee267a194834680d58
Adam Prescott <adam@aprescott.com>
2012-12-21 19:58:56 +0000

    Wrap spec its in a describe.

--- test/site_generation_spec.rb
+++ test/site_generation_spec.rb
@@ -9,16 +9,18 @@ describe Serif::Site do
     FileUtils.rm_rf(testing_dir("_site"))
   end
 
-  it "uses the permalinks in the config file for site generation" do
-    subject.generate
-    File.exist?(testing_dir("_site/test-blog/sample-post.html")).should be_true
-  end
+  describe "site generation" do
+    it "uses the permalinks in the config file for site generation" do
+      subject.generate
+      File.exist?(testing_dir("_site/test-blog/sample-post.html")).should be_true
+    end
 
-  it "reads the layout header for a non-post file and uses the appropriate layout file" do
-    subject.generate
+    it "reads the layout header for a non-post file and uses the appropriate layout file" do
+      subject.generate
 
-    # check it actually got generated
-    File.exist?(testing_dir("_site/page-alt-layout.html")).should be_true
-    File.read("_site/page-alt-layout.html").lines.first.should =~ /<h1.+?>Alternate layout<\/h1>/
+      # check it actually got generated
+      File.exist?(testing_dir("_site/page-alt-layout.html")).should be_true
+      File.read("_site/page-alt-layout.html").lines.first.should =~ /<h1.+?>Alternate layout<\/h1>/
+    end
   end
 end
